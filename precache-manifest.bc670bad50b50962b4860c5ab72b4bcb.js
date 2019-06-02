a469da99f9715a84d9586915748bc88f0bfa7cdd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-02 04:02:10 +0000

    Deploy U-trust/Frontend to github.com/U-trust/Frontend.git:gh-pages

---
+++ precache-manifest.bc670bad50b50962b4860c5ab72b4bcb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e8608b5d8b80c15e81b5ae3c8d4530",
+    "url": "/Frontend/index.html"
+  },
+  {
+    "revision": "4143d3bb385dcbe176f4",
+    "url": "/Frontend/static/css/main.da518d63.chunk.css"
+  },
+  {
+    "revision": "d3dea23e52f8ccd23b08",
+    "url": "/Frontend/static/js/2.43e6d800.chunk.js"
+  },
+  {
+    "revision": "4143d3bb385dcbe176f4",
+    "url": "/Frontend/static/js/main.a62c1a8f.chunk.js"
+  },
+  {
+    "revision": "abdcdd426c6e55f71f7f",
+    "url": "/Frontend/static/js/runtime~main.57aafa16.js"
+  },
+  {
+    "revision": "d97e9b177c7b2bea8f65ce9aba203739",
+    "url": "/Frontend/static/media/bg_profile_1.d97e9b17.png"
+  },
+  {
+    "revision": "06efa87d66137cb23cfec46419f5057e",
+    "url": "/Frontend/static/media/bg_profile_2.06efa87d.png"
+  },
+  {
+    "revision": "7dd0d60703012afcbc03c6d6c3287251",
+    "url": "/Frontend/static/media/bg_profile_3.7dd0d607.png"
+  },
+  {
+    "revision": "753f2b29859ff6e8fe41db51f1f7acc3",
+    "url": "/Frontend/static/media/bg_profile_4.753f2b29.png"
+  },
+  {
+    "revision": "f0e46b570fa7b2cd0bf81032025d74a2",
+    "url": "/Frontend/static/media/bg_promotion2.f0e46b57.png"
+  },
+  {
+    "revision": "74a517a962be6b24abbb65aa491a0bb6",
+    "url": "/Frontend/static/media/bg_promotions.74a517a9.png"
+  },
+  {
+    "revision": "64c9b6346e747f2f55cef8c7a0c97727",
+    "url": "/Frontend/static/media/bg_reviews.64c9b634.png"
+  },
+  {
+    "revision": "8214d75edb0a95e350bd0ea7b87efff8",
+    "url": "/Frontend/static/media/ic_arrow_down.8214d75e.svg"
+  },
+  {
+    "revision": "e6ddf1384d77526423e01eb5ad48602e",
+    "url": "/Frontend/static/media/ic_search.e6ddf138.svg"
+  },
+  {
+    "revision": "96b60272eae76e2ce7d9c7146a9d7c7c",
+    "url": "/Frontend/static/media/logo.96b60272.svg"
+  }
+]);
